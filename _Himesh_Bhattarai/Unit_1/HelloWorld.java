0bd2a290223c9632c52831c6d53a1ed684d13bd1
Himesh Bhattarai <himesh.hcb@gmail.com>
2026-03-29 12:28:47 +0545

    status(201): HelloWorld.java - First Java program with basic syntax

---
+++ _Himesh_Bhattarai/Unit_1/HelloWorld.java
@@ -0,0 +1,21 @@
+// Unit 1: Java Basics - Hello World
+// Concept: First Java program structure
+
+public class HelloWorld {
+
+    // main() is the entry point of Java program
+    // String[] args receives command line arguments
+    // static means it doesn't need an object to run
+    public static void main(String[] args) {
+
+        // print() outputs text without new line
+        System.out.print("Hello ");
+
+        // println() outputs text with new line
+        System.out.println("World!");
+
+        // Variables store data
+        String name = "Himesh";
+        System.out.println("Name: " + name);
+    }
+}
